cc10ed02d728e694155746f774a8c1dd0b637db2
Luke Mannell <luke.c.mannell@gmail.com>
2020-03-29 18:22:49 +0100

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,7 +1,26 @@
 console.log("Up and Running!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "king";
-let cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
+var cardOne = cards[0]
+cardsInPlay.push('cardOne');
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardTwo = cards[1]
+cardsInPlay.push('cardTwo');
+console.log("User Flipped King!");
+
+
+
+
+
+
+// Notations to help me with syntax//
+//if (condition) {
+// Take this path when the condition is true!
+//} else {
+// Take the other path when the condition is false!
+//}
+//if (grade >= 90) {
+//console.log("Student received an A.");
+//} else if (grade >= 80) {
+//console.log("Student received a B");
+//}
