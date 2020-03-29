51b9e9e57489372b47b148dadc9f221d23316417
Luke Mannell <luke.c.mannell@gmail.com>
2020-03-29 18:50:10 +0100

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -7,10 +7,19 @@ console.log("User flipped " + cardOne);
 var cardTwo = cards[1]
 cardsInPlay.push('cardTwo');
 console.log("User Flipped King!");
+if (cardsInPlay length === 2) {
+ cardsInPlay[0] === cardsInPlay[1];
+ alert("You Found a Match!");
+}
+else (cardsInPlay[0] != cardsInPlay[1]{
+	alert("Sorry, Try Again");
+}
 
 
 
 
+//This is not working how it should,
+// I think Im really close but cant figure it out.
 
 
 // Notations to help me with syntax//
